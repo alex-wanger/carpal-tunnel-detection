79a594083f1b6401bd64179db7f6b8f098c2615f
Alex Wang <alexwang94834@gmail.com>
2025-08-14 14:04:03 -0400

    final project working code

--- driver/buzzer.c
+++ driver/buzzer.c
@@ -3,35 +3,45 @@
 static inline void buzzer_set(bool on)
 {
 #if BUZZER_ACTIVE_HIGH
-    if (on) BUZZER_PORT |=  (1 << BUZZER_BIT);
-    else    BUZZER_PORT &= ~(1 << BUZZER_BIT);
+    if (on)
+        BUZZER_PORT |= (1 << BUZZER_BIT);
+    else
+        BUZZER_PORT &= ~(1 << BUZZER_BIT);
 #else
-    if (on) BUZZER_PORT &= ~(1 << BUZZER_BIT);
-    else    BUZZER_PORT |=  (1 << BUZZER_BIT);
+    if (on)
+        BUZZER_PORT &= ~(1 << BUZZER_BIT);
+    else
+        BUZZER_PORT |= (1 << BUZZER_BIT);
 #endif
 }
 
 void buzzer_init(void)
 {
-    BUZZER_DDR  |= (1 << BUZZER_BIT);
+    BUZZER_DDR |= (1 << BUZZER_BIT);
 #if BUZZER_ACTIVE_HIGH
     BUZZER_PORT &= ~(1 << BUZZER_BIT);
 #else
-    BUZZER_PORT |=  (1 << BUZZER_BIT);
+    BUZZER_PORT |= (1 << BUZZER_BIT);
 #endif
 }
 
-void buzzer_on(void)  { buzzer_set(true);  }
+void buzzer_on(void) { buzzer_set(true); }
 void buzzer_off(void) { buzzer_set(false); }
 
 bool buzzer_should_beep(float x, float y, float z)
 {
-    if (x < BUZZ_THR_X_NEG) return true;
-    if (x > BUZZ_THR_X_POS) return true;
-    if (y > BUZZ_THR_Y_POS) return true;
-    if (y < BUZZ_THR_Y_NEG) return true;
-    if (z > BUZZ_THR_Z_POS) return true;
-    if (z < BUZZ_THR_Z_NEG) return true;
+    if (x < BUZZ_THR_X_NEG)
+        return true;
+    if (x > BUZZ_THR_X_POS)
+        return true;
+    if (y > BUZZ_THR_Y_POS)
+        return true;
+    if (y < BUZZ_THR_Y_NEG)
+        return true;
+    if (z > BUZZ_THR_Z_POS)
+        return true;
+    if (z < BUZZ_THR_Z_NEG)
+        return true;
     return false;
 }
 
